1cc7a55408657d364b0f943735fcb846383dea55
Baala Srinivas K <H377720@IE4LLT3ZT70X2>
2021-06-30 14:15:35 +0530

    Imitial Commit

---
+++ Docker/SampleFortran.f90
@@ -0,0 +1,25 @@
+!  SampleFortran.f90 
+!
+!  FUNCTIONS:
+!  SampleFortran - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: SampleFortran
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program SampleFortran
+
+    implicit none
+
+    ! Variables
+
+    ! Body of SampleFortran
+    print *, 'Hello World'
+
+    end program SampleFortran
+
